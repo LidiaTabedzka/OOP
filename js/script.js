ab5553fd43c506b2c82f97c908ac42b5ecbc1626
Lidia Tabędzka <lidia.tabedzka@gmail.com>
2017-11-15 13:04:05 +0100

    Task 1 Add new class and few objects

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function Phone(brand, price, color, weight) {
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+    this.weight = weight;
+}
+
+Phone.prototype.printInfo = function() {
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + ", the price is " + this.price + "zł and the weight is " + this.weight + "g.");
+}
+
+var samsungGalaxyS6 = new Phone("Samsung", 2000, "black", 138);
+var iPhone6S = new Phone("Apple", 3000, "white", 143);
+var onePlusOne = new Phone("OnePlus", 1500, "blue", 162);
+
+samsungGalaxyS6.printInfo();
+iPhone6S.printInfo();
+onePlusOne.printInfo();
